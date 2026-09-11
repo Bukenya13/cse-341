b2daa96cce471ec18c0f4892bbefd8329e4ac074
Bukenya13 <phem.hancho@gmail.com>
2026-09-12 00:33:53 +0300

    mongo

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const morgan = require("/data/databse");
+const port = process.env.PORT || 3000;
+
+app.use('/', require('./routes'));
+
+
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => { console.log(`Database is listening and node Running on port ${port}`) });
+    }
+});
