9919fab49c7bcb3823df246556a35c50651d5431
Bukenya13 <phem.hancho@gmail.com>
2026-09-12 09:36:46 +0300

    fff

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 
-const morgan = require("/data/databse");
+// Load environment variables from .env so MONGODB_URI is available
+require('dotenv').config();
+
+const mongodb = require('./data/database');
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
